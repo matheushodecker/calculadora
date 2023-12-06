33f6c5e0641373d33d4af7e79cb93b39e9fe68c3
matheushodecker <matheus.hodecker.santos@gmail.com>
2023-12-06 16:59:28 -0300

    numeros no teclado

--- script.js
+++ script.js
@@ -42,3 +42,25 @@ function updateDisplay() {
     displayElement.value = currentInput;
     historyElement.innerText = currentInput;
 }
+document.addEventListener("keypress", (e) => {
+    if(e.key == 0){
+    add_numero(0)}
+    if(e.key == 1){
+    add_numero(1)}
+    if(e.key == 2){
+    add_numero(2)}
+    if(e.key == 3){
+    add_numero(3)}
+    if(e.key == 4){
+    add_numero(4)}
+    if(e.key == 5){
+    add_numero(5)}
+    if(e.key == 6){
+    add_numero(6)}
+    if(e.key == 7){
+    add_numero(7)}
+    if(e.key == 8){
+    add_numero(8)}
+    if(e.key == 9){
+    add_numero(9)}
+})
